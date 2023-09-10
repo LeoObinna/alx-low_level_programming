ecd7fd88c92af3a208879b4b9a1a3b7673c12546
LeoObinna <leoaniude20@gmail.com>
2023-09-10 16:48:11 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,16 @@ int main(void)
 	int a;
 
 	for (a = 0; a < 10; a++)
+	{
 
 	putchar(a + '0');
-	if (a < 10)
+	if (a < 9)
+	{
 	putchar(',');
 	putchar(' ');
+	}
+	}
+
 	putchar('\n');
 	return (0);
 }
