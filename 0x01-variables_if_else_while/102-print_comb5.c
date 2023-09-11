87944c2f4ad94416361366f6e693b5e0946ef4c0
LeoObinna <leoaniude20@gmail.com>
2023-09-10 21:01:14 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,9 +20,11 @@ int main(void)
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
 				if (i != 98 || j != 98)
+				{
 				putchar(',');
 				putchar(' ');
 				}
+			}
 		}
 	}
 	putchar('\n');
